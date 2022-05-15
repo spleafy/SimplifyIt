dea1763ffaba023b63d082d6800c58345f261a9b
Martin Petrov <martinmpetrov@hotmail.com>
2022-05-15 12:18:28 +0300

    Integrating notification's settings page

--- server/api/user/friends/createFriendRequest.ts
+++ server/api/user/friends/createFriendRequest.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import FriendRequest from "../../../models/database/friendRequest";
 import User from "../../../models/database/user";
+import Notification from "../../../models/database/notification";
 import ResponseUser from "../../../models/responseUser";
 import ResponseMessage from "../../../models/responseMessage";
 // Utils
@@ -25,12 +26,37 @@ const createFriendRequest = async (req: Request | any, res: Response) => {
   const userTo = await User.findOne({ _id: req.body.id });
   const userFrom = await User.findOne({ _id: req.id });
 
+  if (!userTo.settings.notification.friendRequest) {
+    const friendRequest = await new FriendRequest({
+      from: req.id,
+      to: req.body.id,
+      userTo: new ResponseUser(userTo).getUser(),
+      userFrom: new ResponseUser(userFrom).getUser(),
+    }).save();
+
+    res.json(new ResponseMessage(200, { friendRequest }));
+    return;
+  }
+
   const friendRequest = await new FriendRequest({
     from: req.id,
     to: req.body.id,
     userTo: new ResponseUser(userTo).getUser(),
     userFrom: new ResponseUser(userFrom).getUser(),
   }).save();
+
+  await new Notification({
+    userId: userTo._id,
+    type: "friendRequest",
+    data: {
+      username: userTo.username,
+      fullname: userTo.fullname,
+      settings: userTo.settings,
+    },
+    opened: false,
+    date: new Date(),
+  }).save();
+
   res.json(new ResponseMessage(200, { friendRequest }));
 };
 
