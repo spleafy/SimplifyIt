25a843c326a41fbfd767462cb90b6dafb392171c
Martin Petrov <martinmpetrov@hotmail.com>
2022-06-12 22:47:52 +0300

    Refactoring UI components 4

--- webclient/src/pages/friends/FriendsOverviewPage.tsx
+++ webclient/src/pages/friends/FriendsOverviewPage.tsx
@@ -40,6 +40,9 @@ const OveviewFriendsPage: FC = () => {
                   <Initials text={friend.fullname} />
                 </h1>
               </div>
+              {/* <div className="px-3 py-1 absolute right-3 top-3 bg-slate-900/20 rounded-lg text-white text-sm">
+                  You
+                </div> */}
               <div className="px-5 py-3 absolute bottom-0">
                 <h1 className="text-white">{friend.username}</h1>
               </div>
